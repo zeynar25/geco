5c4eaf4fd8503b547632eb3ff9466ae568110bb8
nate <ryrgnx@gmail.com>
2025-11-08 17:55:28 +0800

    new ResponseAccount DTO

---
+++ src/main/java/com/example/geco/dto/AccountResponse.java
@@ -0,0 +1,16 @@
+package com.example.geco.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountResponse {
+	String passwordNotice;
+    String surname;
+    String firstName;
+    String email;
+    String contactNumber;
+}
