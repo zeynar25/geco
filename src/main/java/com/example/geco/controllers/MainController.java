c96f21b578f11de31c5b6234cba19a143553c5ef
nate <ryrgnx@gmail.com>
2025-11-08 15:38:38 +0800

    Listed out functions needed

--- src/main/java/com/example/geco/controllers/MainController.java
+++ src/main/java/com/example/geco/controllers/MainController.java
@@ -1,125 +1,276 @@
 package com.example.geco.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.geco.domains.Account;
+import com.example.geco.domains.Attraction;
 import com.example.geco.domains.Booking;
-import com.example.geco.services.AccountService;
-import com.example.geco.services.BookingService;
+import com.example.geco.domains.Faq;
+import com.example.geco.domains.Feedback;
+import com.example.geco.domains.PackageInclusion;
+import com.example.geco.domains.TourPackage;
 
 @RestController
 public class MainController {
-	/*
-	 * entities to add
-	 * - Attractions: id, name, description
-	 * 
-	 * functions to build:
-	 * homepage
-	 * - get attractions
-	 * - get monthly visitors
-	 * - get avg rating
-	 * 
-	 * 
-	 * sign in & sign up
-	 * - login with email and password
-	 * - logout
-	 * - sign in with name, email, password
-	 * 
-	 * 
-	 * about us
-	 * - POST feedback
-	 * - GET feedback by category(facilities, staff&service, attractions, cleanliness, overall experience)
-	 * - EDIT feedback
-	 * - DELETE feedback
-	 * - CRUD FAQ questions
-	 * 
-	 * 
-	 * book visit
-	 * - list out packages
-	 * - list out add ons
-	 * - get adds on that's not included on the chosen package
-	 * - list out discount and their details
-	 * - calendar details
-	 * 	 - month and year paramater
-	 *   - mark each day of that date as available, low, moderate, high, and fully booked
-	 * - booking status of selected day = status, expected visitors, n bookings
-	 * - booking status of selected month = total bookings, available days, busy days
-	 * 
-	 * 
-	 * feedback
-	 * - post feedback
-	 * 
-	 * 
-	 * admin-dashboard
-	 * - current month's booking
-	 * - current month's revenue
-	 * - pending bookings
-	 * - current month's feedback
-	 * 
-	 * admin-dashboard bookings 
-	 * - list of bookings sorted descending by date, can be filtered by status and date
-	 * - searchable by name of the client
-	 * - update a booking's status, can add note for self.
-	 * 
-	 * 
-	 * admin-dashboard financial 
-	 * - total revenue = number in peso
-	 * - confirmed bookings = accepted bookings
-	 * - average booking = per month
-	 * - total bookings
-	 * - monthly revenue bar graph filtered by year
-	 * 
-	 * 
-	 * admin-dashboard trends
-	 * - availed packages = pie graph of packages, filtered by year, month
-	 * - revenue stats = if monthly view, filtered by year; if weekly view, filtered by year and month
-	 * 
-	 * 
-	 * admin-dashboard feedback
-	 * - list feedbacks, can be filtered by categories, and date
-	 * 
-	 * 
-	 * admin-dashboard packages
-	 * - CRUD available packages
-	 * 
-	 * 
-	 * admin-dashboard discount
-	 * - CRUD discount
-	 * 
-	 * 
-	 * admin-dashboard attractions
-	 * - CRUD discount
-	 * 
-	 * 
-	 * my bookings
-	 * - get bookings of the user
-	 * - paying the downpayment for confirmed bookings
-	 * 	- either gcash send proof of payment or pay through gcash api
-	*/
-	
-	@Autowired
-	private BookingService bookingService;
-	
-	@Autowired
-	private AccountService accountService;
-	
+	// Functionalities in home page
 	@GetMapping
-	public String home() {
-        return "Agri-Eco Tourism Park";
+	public HashMap<String, Double> home() {
+		HashMap<String, Double> stats = new HashMap<>();
+        return stats;
     }
 	
+	// functionalities of admin-dashboard bookings
+	@GetMapping("/dashboard/bookings")
+	public void displayDashboardBookings() {
+		
+	}
+
+	// functionalities of admin-dashboard financial
+	@GetMapping("/dashboard/finances")
+	public void displayDashboardFinances() {
+		
+	}
+
+	// functionalities of admin-dashboard trend
+	@GetMapping("/dashboard/trends")
+	public void displayDashboardTrends() {
+		
+	}
+
+	// functionalities of admin-dashboard feedback
+	@GetMapping("/dashboard/feedbacks")
+	public void displayDashboardFeedbacks() {
+		
+	}
+
+	// functionalities of admin-dashboard bookings
+	@GetMapping("/dashboard/packages")
+	public void displayDashboardPackages() {
+		
+	}
+
+	// functionalities of admin-dashboard bookings
+	@GetMapping("/dashboard/attractions")
+	public void displayDashboardAttractions() {
+		
+	}
+	
+	// to implement
+	@PostMapping("/signup")
+	public ResponseEntity<?> addAccount(@RequestBody Account account) {
+		return new ResponseEntity<>(new Account(), HttpStatus.CREATED);
+	}
+	
+	// to implement
 	@PostMapping("/login")
-	public ResponseEntity<Account> addAccount(@RequestBody Account account) {
-		Account savedAccount  = accountService.addAccount(account);
-		return new ResponseEntity<>(savedAccount, HttpStatus.CREATED);
+	public void login(@RequestBody Account account) {
+	}
+	
+	// to implement
+	@PostMapping("/logout")
+	public void logout(@RequestBody Account account) {
+		
+	}
+	
+	// to implement
+	@PostMapping("/attraction")
+	public ResponseEntity<Attraction> addAttraction(@RequestBody Attraction attraction) {
+	        return new ResponseEntity<>(new Attraction(), HttpStatus.CREATED);
+	}
+
+	// to implement
+	@GetMapping("/attraction/{id}")
+	public ResponseEntity<Attraction> getAttraction(@PathVariable int id) {
+		return new ResponseEntity<>(new Attraction(), HttpStatus.OK);
+	}
+
+	// to implement
+	@GetMapping("/attraction")
+	public ResponseEntity<List<Attraction>> getAllAttractions() {
+		List<Attraction> attractions = new ArrayList();
+		return new ResponseEntity<>(attractions, HttpStatus.OK);
+	}
+	
+	// to implement
+	@PutMapping("/attraction/{id}")
+	public ResponseEntity<Attraction> updateAttraction(@RequestBody Attraction attraction) {
+		return new ResponseEntity<>(new Attraction(), HttpStatus.OK);
+	}
+
+	// to implement
+	@DeleteMapping("/attraction/{id}")
+	public ResponseEntity<Attraction> deleteAttraction(@PathVariable int id) {
+		return new ResponseEntity<>(new Attraction(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@PostMapping("/feedback")
+	public ResponseEntity<?> addFeedback(@RequestBody Feedback feedback) {
+		return new ResponseEntity<>(new Feedback(), HttpStatus.CREATED);
+	}
+	
+	// to implement
+	@GetMapping("/feedback/{id}")
+	public ResponseEntity<Feedback> getFeedback(@PathVariable int id) {
+		return new ResponseEntity<>(new Feedback(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/feedback/{category}/{year}/{month}")
+	public ResponseEntity<List<Feedback>> getFeedbackByCategory(@PathVariable String category, @PathVariable int year, @PathVariable int month) {
+		List<Feedback> feedbacks = new ArrayList();
+		return new ResponseEntity<>(feedbacks, HttpStatus.OK);
+	}
+	
+	// to implement
+	@PutMapping("/feedback")
+	public ResponseEntity<Feedback> updateFeedback(@RequestBody Feedback feedback) {
+		return new ResponseEntity<>(new Feedback(), HttpStatus.OK);
+	}
+
+	// to implement
+	@DeleteMapping("/feedback/{id}")
+	public ResponseEntity<Feedback> deleteFeedback(@PathVariable int id) {
+		return new ResponseEntity<>(new Feedback(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@PostMapping("/faq")
+	public ResponseEntity<?> addFaq(@RequestBody Faq faq) {
+		return new ResponseEntity<>(new Faq(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/feedback/{id}")
+	public ResponseEntity<Faq> getFaq(@PathVariable int id) {
+		return new ResponseEntity<>(new Faq(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/faq")
+	public ResponseEntity<List<Faq>> getAllFaqs() {
+		List<Faq> faqs = new ArrayList();
+		return new ResponseEntity<>(faqs, HttpStatus.OK);
+	}
+	
+	// to implement
+	@PutMapping("/faq")
+	public ResponseEntity<Faq> updateFaq(@RequestBody Faq faq) {
+		return new ResponseEntity<>(new Faq(), HttpStatus.OK);
+	}
+
+	// to implement
+	@DeleteMapping("/faq/{id}")
+	public ResponseEntity<Faq> deleteFaq(@PathVariable int id) {
+		return new ResponseEntity<>(new Faq(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@PostMapping("/package")
+	public ResponseEntity<?> addPackage(@RequestBody TourPackage tourPackage) {
+		return new ResponseEntity<>(new TourPackage(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/package/{id}")
+	public ResponseEntity<TourPackage> getPackage(@PathVariable int id) {
+		return new ResponseEntity<>(new TourPackage(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/package")
+	public ResponseEntity<List<TourPackage>> getAllPackages() {
+		List<TourPackage> packages = new ArrayList();
+		return new ResponseEntity<>(packages, HttpStatus.OK);
+	}
+	
+	// to implement
+	@PutMapping("/package")
+	public ResponseEntity<TourPackage> updatePackage(@RequestBody TourPackage tourPackage) {
+		return new ResponseEntity<>(new TourPackage(), HttpStatus.OK);
+	}
+
+	// to implement
+	@DeleteMapping("/package/{id}")
+	public ResponseEntity<TourPackage> deletePackage(@PathVariable int id) {
+		return new ResponseEntity<>(new TourPackage(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@PostMapping("/package-inclusion")
+	public ResponseEntity<?> addPackageInclusion(@RequestBody PackageInclusion inclusion) {
+		return new ResponseEntity<>(new PackageInclusion(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/package-inclusion/{id}")
+	public ResponseEntity<PackageInclusion> getPackageInclusion(@PathVariable int id) {
+		return new ResponseEntity<>(new PackageInclusion(), HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/package-inclusion/not/{id}")
+	public ResponseEntity<List<PackageInclusion>> getPackageInclusionNotInPackage(@PathVariable int id) {
+		List<PackageInclusion> inclusions = new ArrayList();
+		return new ResponseEntity<>(inclusions, HttpStatus.OK);
+	}
+	
+	// to implement
+	@GetMapping("/package-inclusion")
+	public ResponseEntity<List<PackageInclusion>> getAllPackageInclusions() {
+		List<PackageInclusion> inclusions = new ArrayList();
+		return new ResponseEntity<>(inclusions, HttpStatus.OK);
+	}
+	
+	// to implement
+	@PutMapping("/package-inclusion")
+	public ResponseEntity<PackageInclusion> updatePackageInclusion(@RequestBody PackageInclusion faq) {
+		return new ResponseEntity<>(new PackageInclusion(), HttpStatus.OK);
+	}
+
+	// to implement
+	@DeleteMapping("/package-inclusion/{id}")
+	public ResponseEntity<PackageInclusion> deletePackageInclusion(@PathVariable int id) {
+		return new ResponseEntity<>(new PackageInclusion(), HttpStatus.OK);
+	}
+
+	// to implement
+	@PostMapping("/booking")
+	public ResponseEntity<?> addBooking(@RequestBody Booking booking) {
+		return new ResponseEntity<>(new Booking(), HttpStatus.CREATED);
+	}
+
+	// to implement
+	@GetMapping("/{id}/booking")
+	public ResponseEntity<List<Booking>> getBookingsByUser(@PathVariable("id") String userId) {
+		List<Booking> bookings = new ArrayList();
+		return new ResponseEntity<>(bookings, HttpStatus.OK);
+    }
+	
+	// to implement
+	@PutMapping("/booking/{id}")
+	public ResponseEntity<Booking> updateBooking(@RequestBody Booking booking) {
+		return new ResponseEntity<>(new Booking(), HttpStatus.OK);
+	}
+	
+	// to implement
+	// payment function, either through gcash api or saving a screenshot of proof of payment
+	
+	// to implement
+	@GetMapping("/calendar/{year}/{month}")
+	public ResponseEntity<?> displayCalendar(@PathVariable int year, @PathVariable int month) {
+		return new ResponseEntity<>("calendar", HttpStatus.OK);
 	}
 }
