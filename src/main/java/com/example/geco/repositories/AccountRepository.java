26b8c3cc34cb4bc7c6000fad5f383116d3da76b9
nate <ryrgnx@gmail.com>
2025-11-08 19:10:43 +0800

    changed CrudRepository to JpaRepository

--- src/main/java/com/example/geco/repositories/AccountRepository.java
+++ src/main/java/com/example/geco/repositories/AccountRepository.java
@@ -1,12 +1,12 @@
 package com.example.geco.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.geco.domains.Account;
 
 
 @Repository
-public interface AccountRepository extends CrudRepository<Account, Integer>{
+public interface AccountRepository extends JpaRepository<Account, Integer>{
 
 }
--- src/main/java/com/example/geco/repositories/UserDetailRepository.java
+++ src/main/java/com/example/geco/repositories/UserDetailRepository.java
@@ -1,11 +1,11 @@
 package com.example.geco.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.geco.domains.UserDetail;
 
 @Repository
-public interface UserDetailRepository extends CrudRepository<UserDetail, Integer>{
+public interface UserDetailRepository extends JpaRepository<UserDetail, Integer>{
 
 }
