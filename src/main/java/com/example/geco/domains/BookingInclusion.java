c6c411b5ed19fab97b4c48f639bbe4e9c030dd52
nate <ryrgnx@gmail.com>
2025-11-11 17:16:56 +0800

    BookingInclusion CRUD

--- src/main/java/com/example/geco/domains/BookingInclusion.java
+++ src/main/java/com/example/geco/domains/BookingInclusion.java
@@ -6,8 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="booking_inclusion")
 public class BookingInclusion {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
